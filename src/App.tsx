5fdc7af8060becc1e5ef40ab12291b9e2cb835c6
Andrey Felipe Cubas <felipedreycubascardoso@gmail.com>
2025-04-03 21:32:31 -0300

    Adiciona funcionalidade de lista de tarefas ao componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    if (newTask.trim() === "") return;
+    if (tasks.includes(newTask)) return; // Evita duplicação de tarefas
+    setTasks([...tasks, newTask]);
+    setNewTask(""); // Limpa o campo de entrada
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter"){
+      addTask();
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>
+        To-Do-List
+      </h2>
+      <input 
+        type="text"
+        value={newTask}
+        onChange={(event) => setNewTask(event.target.value)}
+        placeholder="Digite uma Tarefa"
+        onKeyUp={handleKeyDown}
+        
+      />
+      <button onClick={addTask}>Adicionar</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
+
+/*Nessas linhas acima o codigo esta criando um componente funcional denomidado App, apos isso ele cria um estado chamado tasks que é um array de strings, e por fim ele retorna um elemento div com um h2 dentro dele com o texto To-Do-List*/
